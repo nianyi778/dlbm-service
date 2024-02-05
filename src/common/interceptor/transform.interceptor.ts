5fd1489becbc05042979701abbf694562736800b
nianyi <likai_360@163.com>
2024-02-05 23:19:39 +0800

    feat: merge res template

--- src/common/interceptor/transform.interceptor.ts
+++ src/common/interceptor/transform.interceptor.ts
@@ -6,16 +6,11 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { mergeResTemplate } from 'utils/util';
 
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((data) => ({
-        code: 200,
-        data,
-        message: 'success',
-      })),
-    );
+    return next.handle().pipe(map((data) => mergeResTemplate({ data })));
   }
 }
