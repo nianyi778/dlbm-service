ece6a7961fd705b7aad4957db6a3dc6a1e69a43b
nianyi <likai_360@163.com>
2024-02-24 12:18:43 +0800

    feat: taobao 不鉴权

--- src/taobao/taobao.controller.ts
+++ src/taobao/taobao.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { TaobaoService } from './taobao.service';
 import { MaterialRecommendQuery } from './taobao.type';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
 
 @Controller('taobao')
 export class TaobaoController {
@@ -10,7 +10,7 @@ export class TaobaoController {
   }
 
   @Get('/material/recommend')
-  @UseGuards(AuthGuard('jwt'))
+  // @UseGuards(AuthGuard('jwt'))
   async getData(@Query() query: MaterialRecommendQuery): Promise<unknown[]> {
     const { pageSize = 1, pageNum = 20 } = query;
     return await this.moduleService.getData({ pageSize, pageNum });
